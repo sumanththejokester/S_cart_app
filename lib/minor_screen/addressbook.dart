f6f8a13ee16ca097c1931b35a432794077ec3783
sumanth <sumanth.badugu07@gmail.com>
2022-07-09 13:04:08 +0530

    init

--- lib/minor_screen/addressbook.dart
+++ lib/minor_screen/addressbook.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:multi_store_app/custumer_profile_components/addaddressscreen.dart';
 import 'package:multi_store_app/widgets/appbarwidgets.dart';
 import 'package:multi_store_app/widgets/button.dart';
+import 'package:sn_progress_dialog/progress_dialog.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
@@ -20,6 +21,53 @@ class _AddressBookState extends State<AddressBook> {
       .doc(FirebaseAuth.instance.currentUser!.uid)
       .collection('address')
       .snapshots();
+
+  Future defaultAddressFalse(dynamic item) async {
+    await FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentReference documentReference = FirebaseFirestore.instance
+          .collection('custumers')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .collection('address')
+          .doc(item.id);
+      transaction.update(documentReference, {'default': false});
+    });
+  }
+
+  Future defaultAddressTrue(dynamic custumer) async {
+    await FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentReference documentReference = FirebaseFirestore.instance
+          .collection('custumers')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .collection('address')
+          .doc(custumer['addressid']);
+      transaction.update(documentReference, {'default': true});
+    });
+  }
+
+  Future updateProfile(dynamic custumer) async {
+    await FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentReference documentReference = FirebaseFirestore.instance
+          .collection('custumers')
+          .doc(FirebaseAuth.instance.currentUser!.uid);
+
+      transaction.update(documentReference, {
+        'phone': custumer['phone'],
+        'address':
+            '${custumer['country']} , ${custumer['state']} , ${custumer['city']} , ${custumer['faddress']}'
+      });
+    });
+  }
+
+  void showProgress() {
+    ProgressDialog progressDialog = ProgressDialog(context: context);
+    progressDialog.show(max: 100, msg: 'In Progress');
+  }
+
+  void hideProgress() {
+    ProgressDialog progressDialog = ProgressDialog(context: context);
+    progressDialog.close();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,106 +110,101 @@ class _AddressBookState extends State<AddressBook> {
                   itemCount: snapshot.data!.docs.length,
                   itemBuilder: (context, index) {
                     var custumer = snapshot.data!.docs[index];
-                    return GestureDetector(
-                      onTap: () async {
-                        for (var item in snapshot.data!.docs) {
-                          await FirebaseFirestore.instance
+                    return Dismissible(
+                      key: UniqueKey(),
+                      onDismissed: ((direction) async => await FirebaseFirestore
+                              .instance
                               .runTransaction((transaction) async {
-                            DocumentReference documentReference =
-                                FirebaseFirestore.instance
-                                    .collection('custumers')
-                                    .doc(FirebaseAuth.instance.currentUser!.uid)
-                                    .collection('address')
-                                    .doc(item.id);
-                            transaction
-                                .update(documentReference, {'default': false});
-                          });
-                        }
-                        await FirebaseFirestore.instance
-                            .runTransaction((transaction) async {
-                          DocumentReference documentReference =
-                              FirebaseFirestore.instance
-                                  .collection('custumers')
-                                  .doc(FirebaseAuth.instance.currentUser!.uid)
-                                  .collection('address')
-                                  .doc(custumer['addressid']);
-                          transaction
-                              .update(documentReference, {'default': true});
-                        });
-                        await FirebaseFirestore.instance
-                            .runTransaction((transaction) async {
-                          DocumentReference documentReference =
-                              FirebaseFirestore.instance
-                                  .collection('custumers')
-                                  .doc(FirebaseAuth.instance.currentUser!.uid);
-
-                          transaction.update(documentReference, {
-                            'address':
-                                '${custumer['country']} , ${custumer['state']} , ${custumer['city']} , ${custumer['faddress']}'
-                          });
-                        });
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            //    boxShadow: [
-                            //    BoxShadow(
-                            //    color: Colors.red.withOpacity(0.4),
-                            //  spreadRadius: 2,
-                            //blurRadius: 8,
-                            //),
-                            //],
-                            //color: Colors.blueGrey[100],
-                            gradient: LinearGradient(colors: [
-                              Colors.blueGrey.withOpacity(0.4),
-                              Color.fromARGB(255, 15, 113, 162)
-                                  .withOpacity(0.4),
-                            ]),
-                            border: Border.all(
-                                color: const Color.fromARGB(255, 38, 50, 56)),
-                          ),
-                          height: MediaQuery.of(context).size.height * 0.17,
-                          width: MediaQuery.of(context).size.width * 0.9,
-                          child: ListTile(
-                            trailing: custumer['default'] == true
-                                ? Icon(Icons.home_sharp)
-                                : null,
-                            title: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      '${custumer['firstname']} ${custumer['lastname']}'
-                                          .toUpperCase(),
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w600,
-                                          color:
-                                              Color.fromARGB(255, 14, 55, 15)),
-                                    ),
-                                    Text(
-                                      custumer['phone'],
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w400,
-                                          color:
-                                              Color.fromARGB(255, 112, 19, 12)),
-                                    )
-                                  ],
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                        '${custumer['country']}, ${custumer['state']}'),
-                                    Text(
-                                        '${custumer['city']}, ${custumer['faddress']}'),
-                                  ],
-                                ),
-                              ],
+                            DocumentReference docReference = FirebaseFirestore
+                                .instance
+                                .collection('custumers')
+                                .doc(FirebaseAuth.instance.currentUser!.uid)
+                                .collection('address')
+                                .doc(custumer['addressid']);
+                            transaction.delete(docReference);
+                          })),
+                      child: GestureDetector(
+                        onTap: () async {
+                          showProgress();
+                          for (var item in snapshot.data!.docs) {
+                            await defaultAddressFalse(item);
+                          }
+                          await defaultAddressTrue(custumer).whenComplete(
+                              () async => await updateProfile(custumer));
+                          Future.delayed(Duration(microseconds: 100))
+                              .whenComplete(() => Navigator.pop(context));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              //    boxShadow: [
+                              //    BoxShadow(
+                              //    color: Colors.red.withOpacity(0.4),
+                              //  spreadRadius: 2,
+                              //blurRadius: 8,
+                              //),
+                              //],
+                              //color: Colors.blueGrey[100],
+                              gradient: custumer['default'] == true
+                                  ? LinearGradient(colors: [
+                                      Color.fromARGB(255, 15, 113, 162)
+                                          .withOpacity(0.4),
+                                      Colors.blueGrey.withOpacity(0.4),
+                                    ])
+                                  : LinearGradient(colors: [
+                                      Colors.blueGrey.withOpacity(0.4),
+                                      Color.fromARGB(255, 15, 113, 162)
+                                          .withOpacity(0.4),
+                                    ]),
+                              border: Border.all(
+                                  color: const Color.fromARGB(255, 38, 50, 56)),
+                            ),
+                            height: MediaQuery.of(context).size.height * 0.17,
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: ListTile(
+                              trailing: custumer['default'] == true
+                                  ? Icon(Icons.home_sharp)
+                                  : null,
+                              title: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '${custumer['firstname']} ${custumer['lastname']}'
+                                            .toUpperCase(),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            color: Color.fromARGB(
+                                                255, 14, 55, 15)),
+                                      ),
+                                      Text(
+                                        custumer['phone'],
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            color: Color.fromARGB(
+                                                255, 112, 19, 12)),
+                                      )
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                          '${custumer['country']}, ${custumer['state']}'),
+                                      Text(
+                                          '${custumer['city']}, ${custumer['faddress']}'),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
